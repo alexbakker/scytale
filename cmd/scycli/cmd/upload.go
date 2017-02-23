bd8fca93accb376657df775347db8dbbc69e652b
Impyy <alex@impy.me>
2017-02-23 20:45:47 +0100

    Support stdin as file input for scycli

--- cmd/scycli/cmd/upload.go
+++ cmd/scycli/cmd/upload.go
@@ -7,8 +7,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"os/exec"
+	"path"
 
 	"github.com/Impyy/scytale"
 	"github.com/Impyy/scytale/crypto"
@@ -33,13 +33,24 @@ var (
 
 func init() {
 	RootCmd.AddCommand(uploadCmd)
-	uploadCmd.Flags().BoolVarP(&uploadCmdFlags.Encrypt, "encrypt", "e", false, "Encrypt the file before upload")
-	uploadCmd.Flags().StringVarP(&uploadCmdFlags.File, "file", "f", "", "The file to encrypt and upload")
-	uploadCmd.Flags().BoolVarP(&uploadCmdFlags.Open, "open", "o", false, "Open the result with xdg-open")
+	uploadCmd.Flags().BoolVarP(&uploadCmdFlags.Encrypt, "encrypt", "e", false, "Encrypt the file before upload.")
+	uploadCmd.Flags().StringVarP(&uploadCmdFlags.File, "file", "f", "", "The file to encrypt and upload. Pass - to read from stdin")
+	uploadCmd.Flags().BoolVarP(&uploadCmdFlags.Open, "open", "o", false, "Open the result with xdg-open.")
 }
 
 func startUpload(cmd *cobra.Command, args []string) {
-	bytes, err := ioutil.ReadFile(uploadCmdFlags.File)
+	var keyString string
+	encrypt := uploadCmdFlags.Encrypt
+	req := &scytale.UploadRequest{IsEncrypted: encrypt}
+	filename := uploadCmdFlags.File
+
+	if filename == "-" {
+		filename = "/dev/stdin"
+	} else if !encrypt {
+		req.Extension = path.Ext(filename)
+	}
+
+	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
 		logger.Fatalf("read error: %s", err.Error())
 	}
@@ -48,47 +59,34 @@ func startUpload(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	var loc string
-
-	if uploadCmdFlags.Encrypt {
+	if encrypt {
 		key, encryptedBytes, err := crypto.Encrypt(bytes)
 		if err != nil {
 			logger.Fatalf("encrypt error: %s\n", err.Error())
 		}
 
-		res, err := uploadBlob(encryptedBytes, true)
-		if err != nil {
-			logger.Fatalf("upload error: %s\n", err.Error())
-		}
-
-		keyString := base64.URLEncoding.EncodeToString(key[:])
-		loc = fmt.Sprintf("%s%s#%s", cfg.Endpoint, res.Location, keyString)
-	} else {
-		res, err := uploadBlob(bytes, false)
-		if err != nil {
-			logger.Fatalf("upload error: %s\n", err.Error())
-		}
-
-		loc = fmt.Sprintf("%s%s", cfg.Endpoint, res.Location)
+		bytes = encryptedBytes
+		keyString = base64.URLEncoding.EncodeToString(key[:])
 	}
 
-	fmt.Fprintf(os.Stdout, "%s\n", loc)
+	req.Data = base64.StdEncoding.EncodeToString(bytes)
+	res, err := uploadReq(req)
+	if err != nil {
+		logger.Fatalf("upload error: %s\n", err.Error())
+	}
+
+	loc := fmt.Sprintf("%s%s#%s", cfg.Endpoint, res.Location, keyString)
+	logger.Println(loc)
 
 	if uploadCmdFlags.Open {
-		err = exec.Command("xdg-open", loc).Run()
+		err := exec.Command("xdg-open", loc).Run()
 		if err != nil {
 			logger.Fatalf("xdg-open error: %s\n", err.Error())
 		}
 	}
 }
 
-func uploadBlob(data []byte, isEncrypted bool) (*scytale.UploadResponse, error) {
-	req := &scytale.UploadRequest{
-		IsEncrypted: isEncrypted,
-		Extension:   ".png",
-		Data:        base64.StdEncoding.EncodeToString(data),
-	}
-
+func uploadReq(req *scytale.UploadRequest) (*scytale.UploadResponse, error) {
 	reqBuff := new(bytes.Buffer)
 	err := json.NewEncoder(reqBuff).Encode(req)
 	if err != nil {
