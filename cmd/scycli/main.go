382eca4b66ed92e61c3bfa72fd39b4e21446daf3
Impyy <alex@impy.me>
2017-02-24 17:04:05 +0100

    Remove unused flag definitions

--- cmd/scycli/main.go
+++ cmd/scycli/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 
 	"github.com/Impyy/scytale/cmd/scycli/cmd"
-)
-
-var (
-	flagFile    = flag.String("file", "", "file to encrypt and upload")
-	flagMode    = flag.String("mode", "u", "mode to use (u (upload) or d (download))")
-	flagEncrypt = flag.Bool("encrypt", true, "whether to use encryption or not")
-	flagOpen    = flag.Bool("open", false, "whether to open the result with xdg-open or not")
 )
 
 func main() {
