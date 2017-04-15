9aaf1d9d35903d59b0129e197f87a77de8849978
Impyy <alex@impy.me>
2017-04-15 19:32:12 +0200

    Switch from crypto_secretbox to AES GCM
    AES is more widely implemented

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,41 +1,57 @@
 // Package crypto is only meant to be used by Scytale.
 //
 // The functions in this package are not meant to be fool-proof abstractions for
-// the underlying secretbox API. Encrypt and Decrypt always use 0 as the nonce.
+// the underlying crypto API. Encrypt and Decrypt always use 0 as the nonce.
 // This is acceptable for Scytale because the key is only used once. In other
 // scenarios, this could completely compromise the security of your application.
 // Please don't use this in your own application.
 package crypto
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
-
-	"golang.org/x/crypto/nacl/secretbox"
 )
 
 const (
-	KeySize   = 32
-	nonceSize = 24
+	KeySize = 32
 )
+
+func createCipher(key *[KeySize]byte) (cipher.AEAD, []byte, error) {
+	block, err := aes.NewCipher(key[:])
+	if err != nil {
+		return nil, nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	return gcm, nonce, nil
+}
 
 // Encrypt encrypts the given data with a randomly generated key. The nonce is
 // set to 0.
-func Encrypt(in []byte) (*[KeySize]byte, []byte, error) {
-	nonce := new([nonceSize]byte)
+func Encrypt(data []byte) (*[KeySize]byte, []byte, error) {
 	key := new([KeySize]byte)
-
 	_, err := rand.Read(key[:])
 	if err != nil {
 		return nil, nil, err
 	}
 
-	out := secretbox.Seal(nil, in, nonce, key)
-	return key, out, nil
+	gcm, nonce, err := createCipher(key)
+	return key, gcm.Seal(nil, nonce, data, nil), nil
 }
 
 // Decrypt decrypts the given data with the given key. The nonce is expected to
 // be 0.
-func Decrypt(key *[KeySize]byte, in []byte) ([]byte, bool) {
-	nonce := new([nonceSize]byte)
-	return secretbox.Open(nil, in, nonce, key)
+func Decrypt(key *[KeySize]byte, data []byte) ([]byte, error) {
+	gcm, nonce, err := createCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return gcm.Open(nil, nonce, data, nil)
 }
